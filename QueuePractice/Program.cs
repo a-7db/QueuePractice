59921e61a8da416ead5c785e471772043c36fca3
a-7db <Ahmed97874@gmail.com>
2024-01-24 03:11:33 +0300

    Add project files.

---
+++ QueuePractice/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueuePractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue Qu = new Queue(5);
+            Qu.EnQueue(5);
+            Qu.EnQueue(10);
+            Qu.EnQueue(15);
+
+            Qu.Printing();
+            Console.WriteLine();
+
+            Console.WriteLine("===================");
+            Qu.DelQueue();
+            Qu.Printing();
+
+            Console.WriteLine("===================");
+            Console.WriteLine(Qu.IsEmpty());
+            Console.WriteLine("===================");
+            Qu.DelQueue();
+            Qu.DelQueue();
+            Console.WriteLine(Qu.IsEmpty());
+
+        }
+    }
+}
---
+++ QueuePractice/Queue.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueuePractice
+{
+    internal class Queue
+    {
+        public int[] arry;
+        public int First;
+        public int End;
+        public int count;
+
+        public Queue(int size)
+        {
+            arry = new int[size];
+            First = 0;
+            End = 0;
+            count = 0;
+        }
+
+        public void EnQueue(int Data)
+        {
+            arry[End] = Data;
+            End++;
+            count++;
+        }
+
+        public int DelQueue()
+        {
+            if (count == 0)
+            {
+                Console.WriteLine("You don't have data..");
+                return 0;
+            }
+            else
+            {
+                int temp = First;
+                First++;
+                count--;
+                return temp;
+            }
+        }
+
+
+        public void Printing()
+        {
+            int temp = First;
+            for (int i = temp; i < count; i++)
+            {
+                temp++;
+                Console.WriteLine(arry[i]);
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            if (count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
